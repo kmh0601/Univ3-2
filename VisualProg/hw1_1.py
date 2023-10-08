316520b626e173cc7e60f7c86981430bf1280547
kmh0601 <pongkmh@naver.com>
2023-10-08 21:00:42 +0900

    VP : 과제 7~11 번

--- VisualProg/hw1_1.py
+++ VisualProg/hw1_1.py
@@ -86,16 +86,141 @@ for i in range(1,21):
 
 
 # 7
+# 7
 import random
 comWin = 0
 myWin = 0
-gameRound = 0
-
-print("(라운드 %d)", gameRound)
-print("컴퓨터가 결정했습니다.")
-my = input("무엇을 내시겠습니까?(가위, 바위, 보)")
+gameRound = 1
+SRP = {1:"가위", 2:"바위",3:"보"}
+print("가위바위보 게임")
 
 while comWin < 3 and myWin < 3:
-    com = random.randint(1,3)
+    print("(라운드 {})" .format(gameRound))
+    while True:
+        print("컴퓨터가 결정했습니다.")
+        com = SRP[random.randint(1,3)]
+        my = input("무엇을 내시겠습니까?(가위, 바위, 보)")
+
+        if my == "가위":
+            if com == "가위":
+                #비김
+                print("비겼습니다. 다시 입력하세요")
+            elif com == "바위":
+                #컴 이김
+                print("컴퓨터는 {}, 당신은 {}, 컴퓨터가 이겼습니다.".format(com, my))
+                comWin += 1
+                break
+            elif com == "보":
+                #컴 짐
+                print("컴퓨터는 {}, 당신은 {}, 당신이 이겼습니다.".format(com,my))
+                myWin += 1
+                break
+
+        elif my == "바위":
+            if com == "바위":
+                #비김
+                print("비겼습니다. 다시 입력하세요")
+            elif com == "보":
+                #컴 이김
+                print("컴퓨터는 {}, 당신은 {}, 컴퓨터가 이겼습니다.".format(com, my))
+                comWin += 1
+                break
+            elif com == "가위":
+                #컴 짐
+                print("컴퓨터는 {}, 당신은 {}, 당신이 이겼습니다.".format(com,my))
+                myWin += 1
+                break
+
+        elif my == "보":
+            if com == "보":
+                #비김
+                print("비겼습니다. 다시 입력하세요")
+            elif com == "가위":
+                #컴 이김
+                print("컴퓨터는 {}, 당신은 {}, 컴퓨터가 이겼습니다.".format(com, my))
+                comWin += 1
+                break
+            elif com == "바위":
+                #컴 짐
+                print("컴퓨터는 {}, 당신은 {}, 당신이 이겼습니다.".format(com,my))
+                myWin += 1
+                break
+        else:
+            print("잘못 입력했습니다. 다시 입력하세요")
+
+    print("성적 : {}승 {}패".format(myWin,comWin))
+    gameRound += 1
+
+if comWin > myWin:
+    print("컴퓨터가 최종으로 이겼습니다!")
+else:
+    print("당신이 최종으로 이겼습니다!")
 
 
+#8
+n = int(input("N?"))
+m = int(input("M?(M-N이 0이상 3이하 이어야 합니다.)"))
+for i in range(1,10):
+    for j in range(n,m+1):
+        print("{}x{}={}".format(j,i,i*j),end="\t\t")
+    print()
+
+# 9
+# 입력받은 문자를 두 칸 뒤의 알파벳으로 교체
+target = list(input("문장 : "))
+for i in range(len(target)):
+    if target[i] != " ":
+        target[i] = chr(ord(target[i])+2)
+print("암호화된 문장 :","".join(target))
+
+# 10
+import random
+import math
+
+mfSize = 15
+distance = math.inf
+mfX = random.randint(20,80)
+mfY = random.randint(20,80)
+
+print("자기장 생성기를 찾으세요(크기{})".format(mfSize))
+while mfSize > 0:
+    myX, myY = map(int,input("좌표는?").split())
+    tempDistance = math.sqrt((mfX-myX)**2 + (mfY-myY)**2)
+
+    if(tempDistance <= 1.5):
+        print("({},{})은 자기장 안입니다. 자기장 생성기를 찾았습니다.(거리 {:.1f})".format(myX,myY,tempDistance))
+        print("자기장 생성기의 좌표는({},{})입니다. 오늘은 치킨이닭!".format(mfX,mfY))
+        break
+    elif(tempDistance <= mfSize):
+        print("({},{})은 자기장 안입니다.".format(myX,myY),end=" ")
+    else:
+        print("({},{})은 자기장 밖입니다.".format(myX,myY),end=" ")
+
+    if(tempDistance > distance):
+        distance = tempDistance
+        print("자기장 생성기에서 멀어졌습니다.(크기{})".format(mfSize))
+    elif(tempDistance < distance):
+        distance = tempDistance
+        print("자기장 생성기에 가까워졌습니다.(크기{})".format(mfSize))
+    else:
+        distance = tempDistance
+        print("자기장 생성기에서 같은 거리입니다.(크기{})".format(mfSize))
+
+    mfSize -= 0.5
+
+if mfSize <= 0:
+    print("자기장 생성기를 찾지못했습니다. 자기장 생성기의 좌표는 ({},{})입니다. 오늘은 굶어야 합니다 ㅠㅠ".format(mfX, mfY))
+
+
+
+# 11
+menuDict = {"국수":6000,"비빔밥":8000,"햄버거":4500,"국밥":7500,"스파게티":11000,"피자":9900}
+count = 0
+menu = input("메뉴? (끝내려면 '종료') ")
+while menu != "종료":
+    if menu in menuDict:
+        count += menuDict[menu]
+    else:
+        print("그런 메뉴는 없습니다.")
+    menu = input("메뉴? (끝내려면 '종료') ")
+print("총액 {}원 입니다.".format(count))
